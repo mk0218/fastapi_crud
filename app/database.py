68ca3717da405800c9c53b6f5648985ccd7831d6
mkkim <minamay1@gmail.com>
2021-04-23 02:29:58 +0900

    Create an empty app & set sqlalchemy

---
+++ app/database.py
@@ -0,0 +1,25 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv(verbose=True)
+
+POSTGRES_USERNAME = os.getenv('POSTGRES_USERNAME', 'fastapi')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'fastapi')
+POSTGRES_HOST = os.getenv('POSTGRES_HOST', '127.0.0.1')
+POSTGRES_PORT = os.getenv('POSTGRES_PORT', 5432)
+POSTGRES_DB = os.getenv('POSTGRES_DB', 'fastapi')
+
+SQLALCHEMY_DATABASE_URL = ("postgresql://",
+                           POSTGRES_USERNAME, ":",
+                           POSTGRES_PASSWORD, "@",
+                           POSTGRES_HOST, ":",
+                           POSTGRES_PORT, "/",
+                           POSTGRES_DB)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
